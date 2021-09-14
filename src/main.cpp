7306ffe815b3d688b9243a50532fcef420f620d1
Mattias Lasersköld <mattias.l.sk@gmail.com>
2021-09-14 21:39:37 +0200

    Add some caching

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,53 @@
 #include "sdlpp/render.hpp"
 #include "sdlpp/ttf.hpp"
 #include "sdlpp/window.hpp"
+#include <cstring>
 #include <filesystem>
 #include <iostream>
+#include <map>
+
+namespace {
+
+struct Screen {
+    int width = 20;
+    int height = 20;
+    int charWidth = 0;
+    int charHeight = 0;
+    ttf::Font font;
+
+    Screen()
+        : font{ttf::Font{"data/UbuntuMono-Regular.ttf", 20}} {
+
+        ttf::init();
+
+        if (!font) {
+            std::cerr << "could not load font\n";
+            std::exit(1);
+        }
+        {
+            auto a = font.renderGlyphBlended('a', {});
+            charWidth = a->w;
+            charHeight = a->h;
+        }
+    }
+
+    std::map<uint32_t, sdl::Texture> textures;
+
+    sdl::Texture *getCharacter(sdl::Renderer &renderer, std::string_view c) {
+        auto str = std::string{c.substr(0, 4)};
+        auto i = uint32_t{0};
+        std::memcpy(&i, str.data(), str.size());
+
+        if (auto f = textures.find(i); f != textures.end()) {
+            return &f->second;
+        }
+
+        auto s = font.renderUTF8Blended(str.c_str(), {255, 255, 255, 255});
+        return &(textures[i] = renderer.createTextureFromSurface(s));
+    }
+};
+
+} // namespace
 
 int main(int argc, char **argv) {
     auto window = sdl::Window{"matrix view",
@@ -27,27 +72,14 @@ int main(int argc, char **argv) {
         std::exit(1);
     }
 
-    ttf::init();
-
-    if (!std::filesystem::exists("data/Ubuntu-Regular.ttf")) {
-        std::cerr << "font does not exist on location\n";
-    }
-
-    auto f = ttf::Font{"data/Ubuntu-Regular.ttf", 100};
+    auto f = ttf::Font{"data/UbuntuMono-Regular.ttf", 20};
 
     if (!f) {
         std::cerr << "could not load font\n";
         std::exit(1);
     }
 
-    auto s = f.renderGlyphBlended('b', {255, 255, 255, 255});
-
-    if (!s) {
-        std::cerr << "could not create surface from glyph\n";
-        std::exit(1);
-    }
-
-    auto texture = renderer.createTextureFromSurface(s);
+    auto screen = Screen{};
 
     for (auto event = sdl::waitEvent(); event.type != SDL_QUIT;
          event = sdl::waitEvent()) {
@@ -66,10 +98,24 @@ int main(int argc, char **argv) {
         renderer.drawColor(30, 30, 30, 255);
         renderer.fillRect();
 
-        auto rect = sdl::Rect{0, 0, s->w, s->h};
+        {
+            auto &texture = screen.textures['a'];
+            auto rect = sdl::Rect{0, 0, screen.charWidth, screen.charHeight};
 
-        texture.colorMod(100, 10, 10);
-        renderer.copy(texture, rect, rect);
+            renderer.drawColor(100, 0, 0);
+            renderer.fillRect(rect);
+            texture.colorMod(0, 10, 10);
+            renderer.copy(texture, rect, rect);
+        }
+        {
+            //            auto &texture = screen.textures['x'];
+            auto texture = screen.getCharacter(renderer, "ö");
+            auto rect = sdl::Rect{
+                0, screen.charHeight, screen.charWidth, screen.charHeight};
+            renderer.fillRect(rect);
+            texture->colorMod(255, 255, 255);
+            renderer.copy(*texture, {}, &rect);
+        }
 
         renderer.present();
     }
